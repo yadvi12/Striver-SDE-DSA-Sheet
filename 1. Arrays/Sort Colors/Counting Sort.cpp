3110cfe5aec86105dc9fea3b9538f1ead4198769
Yadvi Bhalla <60046892+yadvi12@users.noreply.github.com>
2022-01-30 11:13:05 +0530

    Create Counting Sort.cpp

---
+++ 1. Arrays/Sort Colors/Counting Sort.cpp
@@ -0,0 +1,33 @@
+// Link - https://leetcode.com/problems/sort-colors/
+// Time Complexity = O(N) + O(N) ~ O(N) -> Running the loop 2 times
+// Space Complexity = O(1)
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int zero = 0, one = 0, two = 0;
+        int n = nums.size();
+        
+        for(int i=0; i<n; i++){
+            if(nums[i]==0){
+                zero++;
+            }
+            else if(nums[i]==1){
+                one++;
+            }
+            else{
+                two++;
+            }
+        }
+        
+        for(int i=0; i<zero; i++){
+            nums[i] = 0;
+        }
+        for(int i=zero; i<zero+one; i++){
+            nums[i]=1;
+        }
+        for(int i=zero+one; i<zero+one+two; i++){
+            nums[i]=2;
+        }
+    }
+};
