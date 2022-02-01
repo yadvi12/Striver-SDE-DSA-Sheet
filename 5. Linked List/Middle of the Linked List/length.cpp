a3753f92d58584a48a06fbdacbd5eb2ddc3cc727
Yadvi Bhalla <60046892+yadvi12@users.noreply.github.com>
2022-02-01 18:26:39 +0530

    Create length.cpp

---
+++ 5. Linked List/Middle of the Linked List/length.cpp
@@ -0,0 +1,34 @@
+// Link - https://leetcode.com/problems/middle-of-the-linked-list/
+// Time Complexity - O(N)
+// Space Complexity - O(1)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        int length = 0;
+        ListNode* temp = head;
+        while(temp != NULL)
+        {
+            length++;
+            temp = temp->next; 
+        }
+        
+        int count = 0;
+        while(count < length/2)
+        {
+            head = head->next;
+            count++;
+        }
+        return head;
+    }
+};
