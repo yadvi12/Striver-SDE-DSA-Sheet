554a818a04365dc63c331d1ce4e2180907a6d76d
Yadvi Bhalla <60046892+yadvi12@users.noreply.github.com>
2022-02-01 18:48:13 +0530

    Update slowFastPointer.cpp

--- 5. Linked List/Middle of the Linked List/slowFastPointer.cpp
+++ 5. Linked List/Middle of the Linked List/slowFastPointer.cpp
@@ -2,3 +2,31 @@
 // Time Complexity - O(N)
 // Space Complexity - O(1)
 
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next;
+            if(fast->next != NULL)
+            {
+                fast = fast->next;
+            }
+        }
+        return slow;
+    }
+};
