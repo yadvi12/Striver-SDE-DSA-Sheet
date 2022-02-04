a00cc31b4dc7278ab8040492aa92e6c4a7381ed2
Yadvi Bhalla <60046892+yadvi12@users.noreply.github.com>
2022-02-04 18:41:06 +0530

    Update optimized.cpp

--- 6. Linked List Part II/Check if a Linked List is Palindrome or not/optimized.cpp
+++ 6. Linked List Part II/Check if a Linked List is Palindrome or not/optimized.cpp
@@ -1,3 +1,77 @@
 // Link - https://leetcode.com/problems/palindrome-linked-list/
 // Time Complexity - O(N)
 // Space Complexity - O(1)
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    ListNode* reverse(ListNode* head)
+    {
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        ListNode* forward = NULL;
+        
+        while(curr != NULL)
+        {
+            forward = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = forward;
+        }
+        return prev;
+    }
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode* fast = head->next;
+        ListNode* slow = head;
+        
+        while(fast != NULL)
+        {
+            fast = fast->next;
+            if(fast != NULL)
+            {
+                fast = fast->next;
+                slow = slow->next;
+            }
+        }
+        
+      if(head == NULL || head->next == NULL)
+      {
+          return true;
+      }
+       
+        ListNode* temp = slow->next;
+        slow->next = reverse(temp);
+        
+        ListNode* head1 = head;
+        ListNode* head2 = slow->next;
+        
+        while(head2 != NULL)
+        {
+            if(head1->val != head2->val)
+            {
+                temp = slow->next;
+                slow->next = reverse(temp);
+                return false;
+            }
+            else{
+                head1 = head1->next;
+                head2 = head2->next;
+            }
+        }
+        temp = slow->next;
+        slow->next = reverse(temp);
+        return true;
+        
+    }
+};
