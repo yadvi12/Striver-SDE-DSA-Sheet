fe266f1dbbaed2208e026f6282b62cfaf035b8a3
Yadvi Bhalla <60046892+yadvi12@users.noreply.github.com>
2022-02-07 17:47:51 +0530

    Update solution.cpp

--- 13. Stack and Queue/Implement Stack using Arrays/solution.cpp
+++ 13. Stack and Queue/Implement Stack using Arrays/solution.cpp
@@ -1,1 +1,73 @@
+// Link - https://practice.geeksforgeeks.org/problems/implement-stack-using-array/1
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+class MyStack
+{
+private:
+    int arr[1000];
+    int top;
+public:
+    MyStack(){top=-1;}
+    int pop();
+    void push(int);
+};
+
+
+int main()
+{
+
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        MyStack *sq = new MyStack();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            sq->push(a);
+        }else if(QueryType==2){
+            cout<<sq->pop()<<" ";
+
+        }
+        }
+        cout<<endl;
+    }
+}
+// } Driver Code Ends
+
+
+
+//Function to push an integer into the stack.
+void MyStack :: push(int x)
+{
+    
+        top++;
+        arr[top] = x;
+    
+}
+
+//Function to remove an item from top of the stack.
+int MyStack :: pop()
+{
+    if(top >= 0)
+    {
+        int ans = arr[top];
+        top--;
+        return ans;
+    }
+    else
+    {
+        return -1;
+    }
+}
 
