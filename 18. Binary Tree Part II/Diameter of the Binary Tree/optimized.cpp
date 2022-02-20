900bc138b093cee6d92307729d6eadea8f2a1f53
Yadvi Bhalla <60046892+yadvi12@users.noreply.github.com>
2022-02-20 11:35:30 +0530

    Create optimized.cpp

---
+++ 18. Binary Tree Part II/Diameter of the Binary Tree/optimized.cpp
@@ -0,0 +1,43 @@
+// Link - https://leetcode.com/problems/diameter-of-binary-tree/
+// Time Complexity - O(N)
+// Space Complexity - O(height)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+
+public:
+    pair<int, int> diameterOfBinaryTreeFast(TreeNode* root)
+    {
+        if(root == NULL)
+        {
+            pair<int, int> p = make_pair(0,0);
+            return p;
+        }
+        
+        pair<int, int> left = diameterOfBinaryTreeFast(root->left);
+        pair<int, int> right = diameterOfBinaryTreeFast(root->right);
+        
+        int op1 = left.first;
+        int op2 = right.first;
+        int op3 = left.second + right.second;
+        
+        pair<int, int> ans;
+        ans.first = max(op1, max(op2, op3));
+        ans.second = max(left.second, right.second) + 1;
+        return ans;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+           return diameterOfBinaryTreeFast(root).first;
+    }
+
+};
